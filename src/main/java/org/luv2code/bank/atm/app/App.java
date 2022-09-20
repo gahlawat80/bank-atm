cf6b340bdcf526d3a775e693d316283f3f7d7716
gahla <gahlawat80@gmail.com>
2022-09-19 22:31:59 -0400

    initial version

---
+++ src/main/java/org/luv2code/bank/atm/app/App.java
@@ -0,0 +1,13 @@
+package org.luv2code.bank.atm.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
